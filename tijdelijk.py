053421f59fc09c76210e5ecacfac8d291eee19c8
sven straetman <sven.straetman@hotmail.be>
2026-01-10 12:51:22 +0100

    bestand tijdelijke.py les 7.3

--- tijdelijk.py
+++ tijdelijk.py
@@ -4,5 +4,6 @@ mijn_dictionary = {
   "chocolade" : 5 
 }
 aanbieding = mijn_dictionary["aardbei"] * 0.8
+reclame_tekst = (f"Vandaag in de aanbieding: vanille-ijs, 1 liter – slechts € {aanbieding}")
 
-print(aanbieding)
+print(reclame_tekst)
