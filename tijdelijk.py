0215cb04f6c2908a05e43b48c28a6305d9e67030
sven straetman <sven.straetman@hotmail.be>
2026-01-10 17:50:39 +0100

    aanpassing code naar prijzen

--- tijdelijk.py
+++ tijdelijk.py
@@ -1,9 +1,9 @@
-mijn_dictionary = {
+prijzen = {
   "aardbei" : 3,
   "vanille" : 4,
   "chocolade" : 5 
 }
-aanbieding = mijn_dictionary["aardbei"] * 0.8
+aanbieding = prijzen["aardbei"] * 0.8
 reclame_tekst = (f"Vandaag in de aanbieding: vanille-ijs, 1 liter – slechts euro {aanbieding}")
 reclame_tekst2 = reclame_tekst[:63]
 reclame_tekst3 = reclame_tekst2.upper()
