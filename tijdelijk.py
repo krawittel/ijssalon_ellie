cc4f4f20b5a32889c6e0ee708e92cdaa013aa3be
sven straetman <sven.straetman@hotmail.be>
2026-01-10 13:16:49 +0100

    bestand tijdelijke.py les 7.8

--- tijdelijk.py
+++ tijdelijk.py
@@ -8,4 +8,5 @@ reclame_tekst = (f"Vandaag in de aanbieding: vanille-ijs, 1 liter – slechts eu
 reclame_tekst2 = reclame_tekst[:63]
 reclame_tekst3 = reclame_tekst2.upper()
 reclame_tekst4 = reclame_tekst3.split()
-print(reclame_tekst4)
+for el in reclame_tekst4:
+    print(el)
