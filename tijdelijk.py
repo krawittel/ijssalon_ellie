ab846cf31d6924b99ff7f663663077aa2c9e3c4b
sven straetman <sven.straetman@hotmail.be>
2026-01-10 13:23:56 +0100

    bestand tijdelijke.py les 7.10

--- tijdelijk.py
+++ tijdelijk.py
@@ -9,4 +9,7 @@ reclame_tekst2 = reclame_tekst[:63]
 reclame_tekst3 = reclame_tekst2.upper()
 reclame_tekst4 = reclame_tekst3.split()
 for el in reclame_tekst4:
-    print(el.lower())
+    if len(el) >= 5:
+        print(el.upper())
+    else:
+        print(el.lower())
