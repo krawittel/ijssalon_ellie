763a1c2dd5f7d6b2df1e2902abebb48cf573703e
sven straetman <sven.straetman@hotmail.be>
2026-01-10 13:12:33 +0100

    bestand tijdelijke.py les 7.7

--- tijdelijk.py
+++ tijdelijk.py
@@ -7,4 +7,5 @@ aanbieding = mijn_dictionary["aardbei"] * 0.8
 reclame_tekst = (f"Vandaag in de aanbieding: vanille-ijs, 1 liter – slechts euro {aanbieding}")
 reclame_tekst2 = reclame_tekst[:63]
 reclame_tekst3 = reclame_tekst2.upper()
-print(reclame_tekst3)
+reclame_tekst4 = reclame_tekst3.split()
+print(reclame_tekst4)
