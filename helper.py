5b82a9c8870f13d05c5b1734e34de9b8e1b1c170
sven straetman <sven.straetman@hotmail.be>
2026-01-18 15:31:09 +0100

    huiswerk les 10

--- helper.py
+++ helper.py
@@ -15,3 +15,7 @@ def onderstreep(tekst = ""):
     uit.append(tekst)
     uit.append(len(tekst) * "=")
     return uit
+
+def som(data):
+    totaal = sum(data.values())
+    return totaal
---
+++ presentatie.py
@@ -0,0 +1,13 @@
+def presenteer(mijn_dict, totaal):
+    for k, v in mijn_dict.items():
+        print(f"{k}: {v} euro")
+    print("==========================")
+    print(f"totaal : {totaal} euro")
+
+mijn_dict = {
+    "vis": 10,
+    "vlees": 25,
+    "overig": 15
+}
+totaal = 50
+
