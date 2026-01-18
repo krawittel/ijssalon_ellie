74222d6c27bf3a233bc93b772011ae5d77714c85
sven straetman <sven.straetman@hotmail.be>
2026-01-18 13:01:01 +0100

    Les 10

--- helper.py
+++ helper.py
@@ -1,3 +1,7 @@
+def fooi_pp(bedrag , personen):
+    bedrag_pp = bedrag/personen
+    return(f"Het bedrag per persoon is {bedrag_pp} euro")  
+
 def decoreer(tekst=""):
     lengte = len(tekst) + 4
     print()
@@ -5,3 +9,9 @@ def decoreer(tekst=""):
     print(f"* {tekst} *")
     print(lengte * "*")
     print()
+
+def onderstreep(tekst = ""):
+    uit = []
+    uit.append(tekst)
+    uit.append(len(tekst) * "=")
+    return uit
